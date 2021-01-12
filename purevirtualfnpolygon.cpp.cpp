4b4ad5677fef8b15cacd2130d802d0995ac0ff5c
musayibjaved <75721508+musayibjaved@users.noreply.github.com>
2021-01-12 20:35:00 +0530

    Add files via upload

---
+++ purevirtualfnpolygon.cpp.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class polygon
+{
+  public:
+  virtual void calculate_area()= 0;
+};
+class rectangle : public polygon
+{
+     public:
+     void calculate_area()
+     {
+     int width = 2;
+     int height= 3;
+     cout<<"the area of rectangle is = "<<(width*height)<<endl;
+     }
+};
+class triangle : public polygon
+{
+       public:
+       void calculate_area()
+       {
+       int breadth = 4;
+       int height = 5;
+       cout<<"the area of triangle is = "<<(0.5*breadth*height)<<endl;
+       }
+};
+class circle : public polygon
+{
+        public:
+        void calculate_area()
+        {
+        int radius = 7;
+        cout<<"the area of circle is ="<<((3.14)*(radius*radius))<<endl;
+        }
+};
+int main()
+{
+  polygon*p;
+  triangle t;
+  p=&t;
+  p->calculate_area();
+  rectangle r;
+  p=&r;
+  p->calculate_area();
+  circle c;
+  p=&c;
+  p->calculate_area();
+};
+
+
+
